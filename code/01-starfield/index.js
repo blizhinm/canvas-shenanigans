85a89af64299194c872d66203367275924044a88
Maksim Blizhin <blizhinm@gmail.com>
2022-04-05 10:56:08 +0300

    01-starfield: solid color toggle

--- code/01-starfield/index.js
+++ code/01-starfield/index.js
@@ -2,20 +2,23 @@ const SCREEN_WIDTH = window.innerWidth * window.devicePixelRatio;
 const SCREEN_HEIGHT = window.innerHeight * window.devicePixelRatio;
 const SCREEN_HALF_WIDTH = (window.innerWidth * window.devicePixelRatio) / 2;
 const SCREEN_HALF_HEIGHT = (window.innerHeight * window.devicePixelRatio) / 2;
+
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
 canvas.width = SCREEN_WIDTH;
 canvas.height = SCREEN_HEIGHT;
+
 const BACKGROUND_COLOR_SOLID = 'rgba(0,0,0,1)';
 const BACKGROUND_COLOR = 'rgba(0,0,0,0.1)';
 const MAIN_COLOR_SOLID = 'rgba(255,255,255,1)';
 const MAIN_COLOR = 'rgba(255,255,255,0.5)';
+let isSolidColor = false;
+
 /** @type {CanvasRenderingContext2D} */
 const ctx2d = canvas.getContext('2d');
 ctx2d.translate(SCREEN_HALF_WIDTH, SCREEN_HALF_HEIGHT);
 ctx2d.fillStyle = BACKGROUND_COLOR;
 ctx2d.fillRect(-SCREEN_HALF_WIDTH, -SCREEN_HALF_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);
-
 ctx2d.fillStyle = MAIN_COLOR;
 ctx2d.strokeStyle = MAIN_COLOR;
 
@@ -58,7 +61,7 @@ for (let i = 0; i < 10000; i += 1) {
 }
 
 (function render() {
-  if (MOUSE_X <= SCREEN_WIDTH * 0.05) {
+  if (MOUSE_X <= SCREEN_WIDTH * 0.05 || isSolidColor) {
     ctx2d.fillStyle = BACKGROUND_COLOR_SOLID;
     ctx2d.fillRect(-SCREEN_HALF_WIDTH, -SCREEN_HALF_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);
     ctx2d.fillStyle = MAIN_COLOR_SOLID;
@@ -75,3 +78,13 @@ for (let i = 0; i < 10000; i += 1) {
 
   requestAnimationFrame(render);
 })();
+
+window.addEventListener('keypress', ({ code }) => {
+  if (code !== 'Space') {
+    return;
+  }
+
+  isSolidColor = !isSolidColor;
+});
+
+window.addEventListener('dblclick', () => (isSolidColor = !isSolidColor));
